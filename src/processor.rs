a9f63b3171b1e3eb7c5f446e08bc2745f1a7a8f6
Beluga <107982136+Belugadex@users.noreply.github.com>
2022-08-29 15:42:07 +0800

    bugfix: 1. bump seed cononicalization 2. missing empty string check

--- src/processor.rs
+++ src/processor.rs
@@ -11,16 +11,19 @@ use crate::{
         DepositAllTokenTypes, Initialize, Swap,
         SwapInstruction, WithdrawAllTokenTypes,
     },
+    state::{SwapV1, SwapVersion},
 };
 use solana_program::{
-    account_info::{AccountInfo},
+    account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     msg,
     program::invoke_signed,
     program_error::{ProgramError},
+    program_option::COption,
     program_pack::Pack,
     pubkey::Pubkey,
 };
+use std::convert::TryInto;
 
 /// Program state handler.
 pub struct Processor {}
@@ -57,8 +60,13 @@ impl Processor {
         my_info: &Pubkey,
         bump_seed: u8,
     ) -> Result<Pubkey, SwapError> {
-        Pubkey::create_program_address(&[&my_info.to_bytes()[..32], &[bump_seed]], program_id)
-            .or(Err(SwapError::InvalidProgramAddress))
+        let (_key, _bump_seed) = Pubkey::find_program_address(&[&my_info.to_bytes()[..32]], program_id);
+        if bump_seed != _bump_seed
+        {
+            return Err(SwapError::InvalidProgramAddress.into());
+        }
+
+        return Ok(_key)
     }
 
     /// Issue a spl_token `Burn` instruction.
@@ -146,12 +154,122 @@ impl Processor {
 
     /// Processes an [Initialize](enum.Instruction.html).
     pub fn process_initialize(
-        _program_id: &Pubkey,
-        _fees: Fees,
-        _swap_curve: SwapCurve,
-        _accounts: &[AccountInfo],
-        _swap_constraints: &Option<SwapConstraints>,
+        program_id: &Pubkey,
+        fees: Fees,
+        swap_curve: SwapCurve,
+        accounts: &[AccountInfo],
+        swap_constraints: &Option<SwapConstraints>,
     ) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+        let swap_info = next_account_info(account_info_iter)?;
+        let authority_info = next_account_info(account_info_iter)?;
+        let token_a_info = next_account_info(account_info_iter)?;
+        let token_b_info = next_account_info(account_info_iter)?;
+        let pool_mint_info = next_account_info(account_info_iter)?;
+        let fee_account_info = next_account_info(account_info_iter)?;
+        let destination_info = next_account_info(account_info_iter)?;
+        let token_program_info = next_account_info(account_info_iter)?;
+
+        let token_program_id = *token_program_info.key;
+        if SwapVersion::is_initialized(&swap_info.data.borrow()) {
+            return Err(SwapError::AlreadyInUse.into());
+        }
+
+        let (swap_authority, bump_seed) =
+            Pubkey::find_program_address(&[&swap_info.key.to_bytes()], program_id);
+        if *authority_info.key != swap_authority {
+            return Err(SwapError::InvalidProgramAddress.into());
+        }
+        let token_a = Self::unpack_token_account(token_a_info, &token_program_id)?;
+        let token_b = Self::unpack_token_account(token_b_info, &token_program_id)?;
+        let fee_account = Self::unpack_token_account(fee_account_info, &token_program_id)?;
+        let destination = Self::unpack_token_account(destination_info, &token_program_id)?;
+        let pool_mint = Self::unpack_mint(pool_mint_info, &token_program_id)?;
+        if *authority_info.key != token_a.owner {
+            return Err(SwapError::InvalidOwner.into());
+        }
+        if *authority_info.key != token_b.owner {
+            return Err(SwapError::InvalidOwner.into());
+        }
+        if *authority_info.key == destination.owner {
+            return Err(SwapError::InvalidOutputOwner.into());
+        }
+        if *authority_info.key == fee_account.owner {
+            return Err(SwapError::InvalidOutputOwner.into());
+        }
+        if COption::Some(*authority_info.key) != pool_mint.mint_authority {
+            return Err(SwapError::InvalidOwner.into());
+        }
+
+        if token_a.mint == token_b.mint {
+            return Err(SwapError::RepeatedMint.into());
+        }
+        swap_curve
+            .calculator
+            .validate_supply(token_a.amount, token_b.amount)?;
+        if token_a.delegate.is_some() {
+            return Err(SwapError::InvalidDelegate.into());
+        }
+        if token_b.delegate.is_some() {
+            return Err(SwapError::InvalidDelegate.into());
+        }
+        if token_a.close_authority.is_some() {
+            return Err(SwapError::InvalidCloseAuthority.into());
+        }
+        if token_b.close_authority.is_some() {
+            return Err(SwapError::InvalidCloseAuthority.into());
+        }
+
+        if pool_mint.supply != 0 {
+            return Err(SwapError::InvalidSupply.into());
+        }
+        if pool_mint.freeze_authority.is_some() {
+            return Err(SwapError::InvalidFreezeAuthority.into());
+        }
+        if *pool_mint_info.key != fee_account.mint {
+            return Err(SwapError::IncorrectPoolMint.into());
+        }
+
+        if let Some(swap_constraints) = swap_constraints {
+            let owner_key = swap_constraints
+                .owner_key
+                .parse::<Pubkey>()
+                .map_err(|_| SwapError::InvalidOwner)?;
+            if fee_account.owner != owner_key {
+                return Err(SwapError::InvalidOwner.into());
+            }
+            swap_constraints.validate_curve(&swap_curve)?;
+            swap_constraints.validate_fees(&fees)?;
+        }
+        fees.validate()?;
+        swap_curve.calculator.validate()?;
+
+        let initial_amount = swap_curve.calculator.new_pool_supply();
+
+        Self::token_mint_to(
+            swap_info.key,
+            token_program_info.clone(),
+            pool_mint_info.clone(),
+            destination_info.clone(),
+            authority_info.clone(),
+            bump_seed,
+            to_u64(initial_amount)?,
+        )?;
+
+        let obj = SwapVersion::SwapV1(SwapV1 {
+            is_initialized: true,
+            bump_seed,
+            token_program_id,
+            token_a: *token_a_info.key,
+            token_b: *token_b_info.key,
+            pool_mint: *pool_mint_info.key,
+            token_a_mint: token_a.mint,
+            token_b_mint: token_b.mint,
+            pool_fee_account: *fee_account_info.key,
+            fees,
+            swap_curve,
+        });
+        SwapVersion::pack(obj, &mut swap_info.data.borrow_mut())?;
         Ok(())
     }
 
@@ -245,3 +363,6 @@ impl Processor {
     }
 }
 
+fn to_u64(val: u128) -> Result<u64, SwapError> {
+    val.try_into().map_err(|_| SwapError::ConversionFailure)
+}
